3f79409da029cda7564988d50e3ccad928d1eb88
Guilherme Jacomini <guilhermem.j@hotmail.com>
2017-10-22 14:04:47 -0200

    Add isEven and countChar functions [PLAYGROUND]
    Eloquent Javascript Exercises, Chapter 3 - Functions

--- playground.js
+++ playground.js
@@ -36,6 +36,139 @@
     return str;
   }
 
-  // Add function to DOM
-  window.createChessBoard = createChessBoard;
+}());
+
+
+/**
+ *  Eloquent Javascript Exercises, Chapter 3 - Recursive Evenness
+ * 
+ *  We’ve seen that % (the remainder operator) can be used to test whether a
+ *  number is even or odd by using % 2 to check whether it’s divisible by two.
+ *
+ *  Here’s another way to define whether a positive whole number is even or odd:
+ *
+ *    - Zero is even.
+ *    - One is odd.
+ *    - For any other number N, its evenness is the same as N - 2.
+ *
+ *  Define a recursive function isEven corresponding to this description.
+ *  The function should accept a number parameter and return a Boolean.
+ *
+ *  Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
+ */
+(function() {
+  "use strict";
+
+  /**
+   *  Check if a number is even recursively.
+   *
+   *  @param {Number} number - Number to be check.
+   *
+   *  @returns {Boolean} - Evenness of number.
+   */
+  function isEven(number) {
+    if (number == 0) return true;
+    if (number == 1) return false;
+
+    return isEven( number + (number > 1 ? -2 : 2) );
+  }
+
+}());
+
+
+/**
+ *  Eloquent Javascript Exercises, Chapter 3 - Bean Counting
+ *
+ *  You can get the Nth character, or letter, from a string by writing "string".charAt(N), similar
+ *  to how you get its length with "s".length. The returned value will be a string containing only
+ *  one character (for example, "b"). The first character has position zero, which causes the last
+ *  one to be found at position string.length - 1. In other words, a two-character string has length 2,
+ *  and its characters have positions 0 and 1.
+ *
+ *  Write a function countBs that takes a string as its only argument and returns a number that indicates
+ *  how many uppercase “B” characters are in the string.
+ *
+ *  Next, write a function called countChar that behaves like countBs, except it takes a second argument
+ *  that indicates the character that is to be counted (rather than counting only uppercase “B” characters).
+ *  Rewrite countBs to make use of this new function.
+ */
+(function() {
+  "use strict";
+  
+  /**
+   *  Count occurrencies of char 'B' in a string.
+   *
+   *  @param {String} str - 'Haystack' string.
+   *
+   *  @returns {Number} - Occurrencies of 'B'.
+   */
+  function countBs(str) {
+    return countChar(str, 'B');
+  }
+
+  /**
+   *  Count occurrencies of given char in a string.
+   *
+   *  @param {String} str   - 'Haystack' string.
+   *  @param {String} char  - Char to be count.
+   *
+   *  @returns {Number} - Occurrencies of `char`.
+   */
+  function countChar(str, char) {
+    var ret = 0;
+
+    for (var i = 0, l = str.length; i < l; i++) {
+      if (str[i] == char) ret++;
+    }
+
+    return ret;
+  }
+
+
+  //  Recursive Approach
+  // ======================
+
+  /**
+   *  Count occurrencies of given char in a string, recursively.
+   *
+   *  @param {String} str   - 'Haystack' string.
+   *  @param {String} char  - Char to be count.
+   *
+   *  @returns {Number} - Occurrencies of `char`.
+   */
+  function countCharRecursively(str, char) {
+    if (!str.length) return 0;
+    
+    return (str[0] == char ? 1 : 0) + countCharRecursively(str.substring(1), char);
+  }
+  
+  /**
+   *  Encapsulates and correctly initiates the recursive, no-substring, function.
+   *
+   *  @param {String} str   - 'Haystack' string.
+   *  @param {String} char  - Char to be count.
+   *
+   *  @returns {Number} - Occurrencies of `char`.
+   */
+  function countCharNoSubstring(str, char) {
+    return trueCountCharNoSubstring(str, char, str.length - 1);
+  }
+  
+  /**
+   *  Count occurrencies of given char in a string, recursively, without using `substring`.
+   *
+   *  @param {String} str   - 'Haystack' string.
+   *  @param {String} char  - Char to be count.
+   *  @param {Number} i     - Index of current char.
+   *
+   *  @returns {Number} - Occurrencies of `char`.
+   *
+   *  @private
+   */
+  function trueCountCharNoSubstring(str, char, i) {
+    if (i < 0) return 0;
+
+    return (str[i] == char ? 1 : 0) + trueCountCharNoSubstring(str, char, i - 1);
+  }
+
 }());
