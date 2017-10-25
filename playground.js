8f1c28e084b75f00cc34179ae50ff234f682f362
Guilherme Jacomini <guilhermem.j@hotmail.com>
2017-10-25 12:44:24 -0200

    Add array and list relative functions [Playground]
    Eloquent Javascript Exercises, Chapter 4 - Data Structures: Objects and
    Arrays

--- playground.js
+++ playground.js
@@ -79,18 +79,18 @@
 /**
  *  Eloquent Javascript Exercises, Chapter 3 - Bean Counting
  *
- *  You can get the Nth character, or letter, from a string by writing "string".charAt(N), similar
- *  to how you get its length with "s".length. The returned value will be a string containing only
- *  one character (for example, "b"). The first character has position zero, which causes the last
- *  one to be found at position string.length - 1. In other words, a two-character string has length 2,
- *  and its characters have positions 0 and 1.
+ *  You can get the Nth character, or letter, from a string by writing "string".charAt(N),
+ *  similar to how you get its length with "s".length. The returned value will be a string
+ *  containing only one character (for example, "b"). The first character has position zero,
+ *  which causes the last one to be found at position string.length - 1. In other words, a
+ *  two-character string has length 2, and its characters have positions 0 and 1.
  *
- *  Write a function countBs that takes a string as its only argument and returns a number that indicates
- *  how many uppercase “B” characters are in the string.
+ *  Write a function countBs that takes a string as its only argument and returns a number that
+ *  indicates how many uppercase “B” characters are in the string.
  *
- *  Next, write a function called countChar that behaves like countBs, except it takes a second argument
- *  that indicates the character that is to be counted (rather than counting only uppercase “B” characters).
- *  Rewrite countBs to make use of this new function.
+ *  Next, write a function called countChar that behaves like countBs, except it takes a second
+ *  argument that indicates the character that is to be counted (rather than counting only uppercase
+ *  “B” characters). Rewrite countBs to make use of this new function.
  */
 (function() {
   "use strict";
@@ -177,19 +177,19 @@
 /**
  *  Eloquent Javascript Exercises, Chapter 4 - The sum of a range
  *
- *  Write a range function that takes two arguments, start and end, and returns an array containing all
- *  the numbers from start up to (and including) end.
+ *  Write a range function that takes two arguments, start and end, and returns an array containing
+ *  all the numbers from start up to (and including) end.
  *
  *  Next, write a sum function that takes an array of numbers and returns the sum of these numbers.
  *  Run the previous program and see whether it does indeed return 55.
  *
- *  As a bonus assignment, modify your range function to take an optional third argument that indicates the
- *  “step” value used to build up the array. If no step is given, the array elements go up by increments of one,
- *  corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure
- *  it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
+ *  As a bonus assignment, modify your range function to take an optional third argument that indicates
+ *  the “step” value used to build up the array. If no step is given, the array elements go up by
+ *  increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should
+ *  return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1)
+ *  produces [5, 4, 3, 2].
  */
 (function() {
-  "use strict";
 
   /**
    *  Create an array with numbers of given range.
@@ -220,4 +220,126 @@
    *  @returns {Number} - Sum of numbers.
    */
   const sum = numberArr => numberArr.reduce( (a, b) => a + b );
+}());
+
+
+/**
+ *  Eloquent Javascript Exercises, Chapter 4 - Reversing an array
+ *
+ *  Arrays have a method reverse, which changes the array by inverting the order in which its elements
+ *  appear. For this exercise, write two functions, reverseArray and reverseArrayInPlace. The first,
+ *  reverseArray, takes an array as argument and produces a new array that has the same elements in the
+ *  inverse order. The second, reverseArrayInPlace, does what the reverse method does: it modifies the
+ *  array given as argument in order to reverse its elements. Neither may use the standard reverse method.
+ *
+ *  Thinking back to the notes about side effects and pure functions in the previous chapter, which variant
+ *  do you expect to be useful in more situations? Which one is more efficient?
+ */
+(function() {
+  const reverseArray = arr => {
+    let revArr = [];
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+      revArr.push(arr[i]);
+    }
+      
+    return revArr;
+  };
+
+  const reverseArrayInPlace = arr => {
+    for (let i = 0, end = arr.length - 1, mid = Math.floor( end / 2 ), aux; i < mid; i++ ) {
+      aux = arr[i];
+      arr[i] = arr[ end - i ];
+      arr[ end - i ] = aux;
+    }
+  };
+}());
+
+
+/**
+ *  Eloquent Javascript Exercises, Chapter 4 - A list
+ *
+ *  Objects, as generic blobs of values, can be used to build all sorts of data structures. A common data
+ *  structure is the list (not to be confused with the array). A list is a nested set of objects, with the
+ *  first object holding a reference to the second, the second to the third, and so on.
+ *
+ *  A nice thing about lists is that they can share parts of their structure. For example, if I create two
+ *  new values {value: 0, rest: list} and {value: -1, rest: list} (with list referring to the variable
+ *  defined earlier), they are both independent lists, but they share the structure that makes up their
+ *  last three elements. In addition, the original list is also still a valid three-element list.
+ *
+ *  Write a function arrayToList that builds up a data structure like the previous one when given [1, 2, 3]
+ *  as argument, and write a listToArray function that produces an array from a list. Also write the helper
+ *  functions prepend, which takes an element and a list and creates a new list that adds the element to
+ *  the front of the input list, and nth, which takes a list and a number and returns the element at the
+ *  given position in the list, or undefined when there is no such element.
+ *
+ *  If you haven’t already, also write a recursive version of nth.
+ */
+(function() {
+
+  /**
+   *  Convert an array to a list.
+   *
+   *  @param {*[]} arr - Array to be converted.
+   *
+   *  @returns {Object} - The list that represents the Array.
+   */
+  const arrayToList = arr => {
+    let list = null;
+
+    for (i = arr.length - 1; i >= 0; i--) {
+      list = {value: arr[i], rest: list};
+    }
+
+    return list;
+  };
+
+  /**
+   *  Prepend an element to a list.
+   *
+   *  @param {*}      elem - Element to be prepended.
+   *  @param {Object} list - List that will receive the new element.
+   *
+   *  @returns {Object} The new list with the prepended element.
+   */
+  const prepend = (elem, list) => {
+    return {
+      value: elem,
+      rest: list
+    };
+  };
+
+  /**
+   *  Convert a list to an array.
+   *
+   *  @param {Object} list - The list to be converted.
+   *
+   *  @returns {*[]} - Array representing the list.
+   */
+  const listToArray = list => {
+    let arr = [];
+
+    for (let node = list; node; node = node.rest) {
+      arr.push(node.value);
+    }
+
+    return arr;
+  };
+  
+  /**
+   *  Recover nth item of given list.
+   *    NOTE: Function declaration must be used to allow recurssion.
+   *
+   *  @param {Object} list  - Input list.
+   *  @param {Number} index - Position of the desired item.
+   *
+   *  @returns {*} - Value of nth item in list.
+   */
+  function nth (list, index) {
+    if (index === 0) return list.value;
+    if (!list.rest || index < 0) return undefined;
+
+    return nth(list.rest, index - 1);
+  };
 }());
