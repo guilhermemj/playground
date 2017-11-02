0a2ffed7e6259324dd610b1495a4e905fb83886d
Guilherme Jacomini <guilhermem.j@hotmail.com>
2017-11-02 12:48:10 -0200

    Add every and some functions [Playground]
    Eloquent Javascript Exercises, Chapter 5 - Higher-Order Functions

--- exercises-eloquent.js
+++ exercises-eloquent.js
@@ -603,3 +603,54 @@
   }
   
 }());
+
+/**
+ *  Eloquent Javascript Exercises, Chapter 5 - Every and then some
+ *
+ *  Arrays also come with the standard methods every and some. Both take a predicate function that,
+ *  when called with an array element as argument, returns true or false. Just like && returns a true
+ *  value only when the expressions on both sides are true, every returns true only when the predicate
+ *  returns true for all elements of the array. Similarly, some returns true as soon as the predicate
+ *  returns true for any of the elements. They do not process more elements than necessary—for example,
+ *  if some finds that the predicate holds for the first element of the array, it will not look at the
+ *  values after that.
+ *
+ *  Write two functions, every and some, that behave like these methods, except that they take the array
+ *  as their first argument rather than being a method.
+ */
+(function() {
+
+  /**
+   *  Test whether all elements in the array pass the test implemented by the provided function.
+   *
+   *  @param {*[]} arr     - Input array.
+   *  @param {Function} fn - Function to test for each element.
+   *
+   *  @returns {Boolean} - `true` if the callback function returns a truthy value for every array element;
+   *                        otherwise, `false`.
+   */
+  const every = (arr, fn) => {
+    for (let i = 0; i < arr.length; i++) {
+      if (!fn( arr[i] )) return false;
+    }
+
+    return true;
+  };
+
+  /**
+   *  Test whether at least one element in the array passes the test implemented by the provided function.
+   *
+   *  @param {*[]} arr     - Input array.
+   *  @param {Function} fn - Function to test for each element.
+   *
+   *  @returns {Boolean} - `true` if the callback function returns a truthy value for any array element;
+   *                        otherwise, `false`.
+   */
+  const some = (arr, fn) => {
+    for (let i = 0; i < arr.length; i++) {
+      if (fn( arr[i] )) return true;
+    }
+
+    return false;
+  };
+}());
