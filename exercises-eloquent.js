3922727f2618a260d5186401ac71a4a020e62135
Guilherme Jacomini <guilhermem.j@hotmail.com>
2017-11-04 13:32:37 -0200

    Implement Vector Class [Playground]
    Eloquent Javascript Exercises, Chapter 6 - The Secret Life of Objects

--- exercises-eloquent.js
+++ exercises-eloquent.js
@@ -657,3 +657,61 @@
     return false;
   };
 }());
+
+/**
+ *  Eloquent Javascript Exercises, Chapter 6 - A vector type
+ *
+ *  Write a constructor Vector that represents a vector in two-dimensional space. It takes x and y parameters
+ *  (numbers), which it should save to properties of the same name.
+ *
+ *  Give the Vector prototype two methods, plus and minus, that take another vector as a parameter and return
+ *  a new vector that has the sum or difference of the two vectors’ (the one in this and the parameter) x and
+ *  y values.
+ *
+ *  Add a getter property length to the prototype that computes the length of the vector — that is, the distance
+ *  of the point (x, y) from the origin (0, 0).
+ */
+(function() {
+
+  const Vector = function(x, y) {
+    this.x = x;
+    this.y = y;
+  };
+
+  Vector.prototype = {
+    
+    plus: function(vector) {
+      return new Vector( this.x + vector.x, this.y + vector.y );
+    },
+
+    minus: function(vector) {
+      return new Vector( this.x - vector.x, this.y - vector.y );
+    },
+
+    get length() {
+      return Math.sqrt( Math.pow(this.x, 2) + Math.pow(this.y, 2) );
+    }
+  }
+
+  //  ES6 Approach
+  // ===============
+  
+  class VectorES6 {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    get length() {
+      return Math.sqrt( Math.pow(this.x, 2) + Math.pow(this.y, 2) );
+    }
+
+    plus(vector) {
+      return new VectorES6( this.x + vector.x, this.y + vector.y );
+    }
+
+    minus(vector) {
+      return new VectorES6( this.x - vector.x, this.y - vector.y );
+    }
+  }
+}());
