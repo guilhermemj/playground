95ce234f6eb739a21ddbbd05e68c5025c09ee95b
Guilherme Jacomini <guilhermem.j@hotmail.com>
2017-12-28 15:48:18 -0200

    Exercises, Chapter 10 - Modules [EloquentJS]

--- exercises-eloquent.js
+++ exercises-eloquent.js
@@ -1497,3 +1497,43 @@
 		["1a", "+-1", "1.2.3", "1+1", "1e4.5", ".5.", "1f5", "."]
 	);
 }());
+
+
+// ================================================
+//  Eloquent Javascript, Chapter 10 - Modules
+//
+//  http://eloquentjavascript.net/10_modules.html
+// ================================================
+(function () {
+
+	// ======================================================================================
+	//  Eloquent Javascript Exercises - Month names
+	//
+	//  Write a simple module similar to the weekDay module that can convert month numbers
+	//  (zero-based, as in the Date type) to names and can convert names back to numbers.
+	//  Give it its own namespace since it will need an internal array of month names, and
+	//  use plain JavaScript, without any module loader system.
+	// ======================================================================================
+
+	const month = () => {
+		const MONTH_NAMES = [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December',
+		];
+
+		return {
+			name: (monthNumber) => MONTH_NAMES[monthNumber],
+			number: (monthName) => MONTH_NAMES.indexOf(monthName),
+		};
+	};
+}());
