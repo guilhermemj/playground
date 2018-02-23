4d088f42dd110c3ae48a3e28622be23b54aaaf51
Guilherme Jacomini <guilhermem.j@hotmail.com>
2018-02-23 14:22:51 -0300

    Ultimate FizzBuzz [EloquentJS]

--- eloquent-js-exercises/3rd Edition/chapter2.js
+++ eloquent-js-exercises/3rd Edition/chapter2.js
@@ -32,7 +32,7 @@
 	for (let str = '#'; str.length <= 7; str += '#') console.log(str);
 
 	// Overkilling version
-	const repeat = (fn, times) => {
+	const repeat = (fn, times = 1) => {
 		for (let counter = 1; counter <= times; counter++) {
 			fn(counter);
 		}
@@ -46,4 +46,67 @@
 	}, 7);
 
 	console.log(triangleString);
+})();
+
+(() => {
+
+	// ==========================================================================================
+	//   FizzBuzz
+	//  -----------
+	//
+	//  Write a program that uses console.log to print all the numbers from 1 to 100, and
+	//  with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number,
+	//  for numbers divisible by 5 (and not 3), print "Buzz" instead.
+	//
+	//  When you have that working, modify your program to print "FizzBuzz", for numbers that
+	//  are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible
+	//  by only one of those).
+	//
+	//  (This is actually an interview question that has been claimed to weed out a significant
+	//  percentage of programmer candidates. So if you solved it, your labor market value just
+	//  went up.)
+	// ==========================================================================================
+
+	// Code Golf Version
+	for (let i = 1; i <= 100; i++) {
+		let str = '';
+
+		if (!(i % 3)) str += 'Fizz';
+		if (!(i % 5)) str += 'Buzz';
+
+		console.log(str || i);
+	}
+
+	// Overkilling Version
+	const isDivisible = (dividend, divider) => !(dividend % divider);
+
+	const createArray = (elementCreator, length) => {
+		let array = [];
+
+		for (let index = 0; index < length; index++) {
+			array.push(elementCreator(index));
+		}
+
+		return array;
+	};
+
+	const defaultDictionary = {
+		3: 'Fizz',
+		5: 'Buzz',
+	};
+
+	const UltimateFizzBuzz = (limit = 10, dictionary = defaultDictionary) => {
+		const createLine = (number) => {
+			const dividers = Object.keys(dictionary).filter((key) => isDivisible(number, Number(key)));
+
+			return (!!dividers.length ?
+				dividers.reduce((line, key) => line + dictionary[key], '') :
+				number
+			);
+		};
+
+		return createArray((index) => createLine(index + 1), limit);
+	};
+
+	console.log(UltimateFizzBuzz(100).join('\n'));
 })();
