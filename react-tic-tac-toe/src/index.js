320965426dbba4c2109463ed6d81c15a11d897fc
Guilherme Jacomini <guilhermem.j@hotmail.com>
2021-12-10 21:31:21 -0300

    feat(tic-tac-toe): Create basic game

--- react-tic-tac-toe/src/index.js
+++ react-tic-tac-toe/src/index.js
@@ -2,23 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
-}
+const Square = (props) => (
+  <button className="square" onClick={props.onClick}>
+    {props.value}
+  </button>
+);
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    if (calculateWinner(this.state.squares) || this.state.squares[i]) {
+      return;
+    }
+
+    const squares = this.state.squares.slice();
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+    this.setState({ squares, xIsNext: !this.state.xIsNext });
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    const nextPlayer = this.state.xIsNext ? 'X' : 'O';
+    const status = winner ? `Winner: ${winner}` : `Next player: ${nextPlayer}`;
 
     return (
       <div>
@@ -57,6 +81,28 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
 }
 
 // ========================================
