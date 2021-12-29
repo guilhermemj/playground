6c6921e90c4cd8b0307d7c7b7d4500a9c41913d5
Guilherme Jacomini <guilhermem.j@hotmail.com>
2021-12-29 02:12:10 -0300

    feat(tic-tac-toe): Highlight current move

--- react-tic-tac-toe/src/index.js
+++ react-tic-tac-toe/src/index.js
@@ -9,7 +9,7 @@ import Game from './components/Game';
   --------------------
 
   [X] Display the location for each move in the format (col, row) in the move history list.
-  [ ] Bold the currently selected item in the move list.
+  [X] Bold the currently selected item in the move list.
   [X] Rewrite Board to use two loops to make the squares instead of hardcoding them.
   [ ] Add a toggle button that lets you sort the moves in either ascending or descending order.
   [ ] When someone wins, highlight the three squares that caused the win.
