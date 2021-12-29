5f246fb50dd98877c90afe64967723c8cf17e30b
Guilherme Jacomini <guilhermem.j@hotmail.com>
2021-12-29 03:10:47 -0300

    feat(tic-tac-toe): Add button to sort history

--- react-tic-tac-toe/src/index.js
+++ react-tic-tac-toe/src/index.js
@@ -11,7 +11,7 @@ import Game from './components/Game';
   [X] Display the location for each move in the format (col, row) in the move history list.
   [X] Bold the currently selected item in the move list.
   [X] Rewrite Board to use two loops to make the squares instead of hardcoding them.
-  [ ] Add a toggle button that lets you sort the moves in either ascending or descending order.
+  [X] Add a toggle button that lets you sort the moves in either ascending or descending order.
   [ ] When someone wins, highlight the three squares that caused the win.
   [ ] When no one wins, display a message about the result being a draw.
 
