3ea8498c8aa1ff0ffd0270202eafe09bf3cb887d
Guilherme Jacomini <guilhermem.j@hotmail.com>
2021-12-29 01:27:32 -0300

    refactor(tic-tac-toe): Rewrite game logic

--- react-tic-tac-toe/src/components/Board/index.js
+++ react-tic-tac-toe/src/components/Board/index.js
@@ -1,37 +1,20 @@
 import React from 'react';
 import Square from '../Square';
 
-export class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+export const Board = (props) => (
+  <div>
+    {props.squares.map((cols, row) => (
+      <div className="board-row" key={row}>
+        {cols.map((value, col) => (
+          <Square
+            key={col}
+            value={value}
+            onClick={() => props.onClick(row, col)}
+          />
+        ))}
       </div>
-    );
-  }
-}
+    ))}
+  </div>
+);
 
 export default Board;
---
+++ react-tic-tac-toe/src/config.js
@@ -0,0 +1,45 @@
+export const PLAYERS = ['X', 'O'];
+export const BOARD_SIZE = '3x3';
+
+export const WINNING_CONDITIONS = [
+  [
+    { row: 0, col: 0 },
+    { row: 0, col: 1 },
+    { row: 0, col: 2 },
+  ],
+  [
+    { row: 1, col: 0 },
+    { row: 1, col: 1 },
+    { row: 1, col: 2 },
+  ],
+  [
+    { row: 2, col: 0 },
+    { row: 2, col: 1 },
+    { row: 2, col: 2 },
+  ],
+  [
+    { row: 0, col: 0 },
+    { row: 1, col: 0 },
+    { row: 2, col: 0 },
+  ],
+  [
+    { row: 0, col: 1 },
+    { row: 1, col: 1 },
+    { row: 2, col: 1 },
+  ],
+  [
+    { row: 0, col: 2 },
+    { row: 1, col: 2 },
+    { row: 2, col: 2 },
+  ],
+  [
+    { row: 0, col: 0 },
+    { row: 1, col: 1 },
+    { row: 2, col: 2 },
+  ],
+  [
+    { row: 0, col: 2 },
+    { row: 1, col: 1 },
+    { row: 2, col: 0 },
+  ],
+];
